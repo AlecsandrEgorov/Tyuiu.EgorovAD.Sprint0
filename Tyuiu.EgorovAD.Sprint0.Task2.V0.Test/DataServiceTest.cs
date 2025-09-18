c4a632ba323c32c05d64cd228f43cd7735034af1
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-09-18 20:08:31 +0300

    Task2

---
+++ Tyuiu.EgorovAD.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using TyuiuEgorovAD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.EgorovAD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Александр";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Александр", res);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using TyuiuEgorovAD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.EgorovAD.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
