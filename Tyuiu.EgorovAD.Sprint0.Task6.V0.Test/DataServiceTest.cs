81d04bfb8fedc644d887796106573d455795dffb
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-09-19 19:21:10 +0300

    Task6

---
+++ Tyuiu.EgorovAD.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.EgorovAD.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.EgorovAD.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numders = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numders);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void CheckSubtractionArrayValid()
+        {
+            var numders = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numders);
+            Assert.AreEqual(-15, res);
+        }
+
+        [TestMethod]
+        public void CheckMultiplicationArrayValid()
+        {
+            var numders = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numders);
+            Assert.AreEqual(120, res);
+        }
+
+    }
+
+}
